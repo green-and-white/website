c7f219e10b0f74771e8636e3c2a81f8698db0d22
Johan Marlo Cabili <jmtcabili@gmail.com>
2025-08-30 00:16:14 +0800

    feat: installed tailwindcss and added sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,27 @@
 import { useState } from 'react'
+import Hero from './sections/Hero'
+import About from './sections/About'
+import Committees from './sections/Committees'
+import Process from './sections/Process'
+import FAQs from './sections/FAQs'
+import Footer from './sections/Footer'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <Hero></Hero>
+      <About></About>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Committees></Committees>
+        <Process></Process>
+        <FAQs></FAQs>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Footer></Footer>
+  
     </>
   )
 }
