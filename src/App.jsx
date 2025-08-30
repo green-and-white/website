a04c31aea7029a77e1b8d89d26aa40213bf4c848
Johan Marlo Cabili <jmtcabili@gmail.com>
2025-08-30 23:35:56 +0800

    feat: logo and base hero section

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,13 @@ import Committees from './sections/Committees'
 import Process from './sections/Process'
 import FAQs from './sections/FAQs'
 import Footer from './sections/Footer'
-import FontTest from './FontTest'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <FontTest></FontTest>
       <Hero></Hero>
       <About></About>
       <div>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,37 @@
+import logo from "/src/assets/icons/logo.svg";
+
 export default function Hero() {
   return (
-    <div className = "border">
-        Hero
-    </div>
-  )
+    <section className="relative bg-radial from-blue-900 from-11% to-blue-800 to-100% min-h-screen">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8 h-full">
+        <div className="flex flex-col justify-between items-center text-center h-screen py-8">
+          
+            {/* Top: Logo + Org Name + Tagline */}
+            <div className="relative flex flex-col h-1/2 md:justify-end md:bottom-6 items-center gap-2">
+                <img src={logo} alt="Logo" className="max-h-18" />
+
+                <h2 className="font-waltograph text-white text-2xl md:text-3xl font-bold tracking-wide">
+                    Green &amp; White
+                </h2>
+
+                <p className="font-montserrat text-white text-xs italic">
+                    CAPTURING IMAGES OF LASALLIAN EXCELLENCE
+                </p>
+            </div>
+            <div className = "flex flex-col h-1/2 justify-between">
+                {/* Middle: Headline */}
+                <h1 className="text-4xl font-insideout font-bold tracking-wider text-white md:text-7xl">
+                    Make A Core Memory
+                </h1>
+
+                {/* Bottom: Tagline */}
+                <p className="text-white text-center text-xs md:text-lg font-pixar">
+                    ANNUAL RECRUITMENT WEEK
+                </p>
+            </div>
+         
+        </div>
+      </div>
+    </section>
+  );
 }
