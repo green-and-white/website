79513f2bbb6e798eba6902bbe22cb612672cb594
Johan Marlo Cabili <jmtcabili@gmail.com>
2025-10-20 14:58:30 +0800

    chore: uploaded placeholder bg assets for home page and retro color palette

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Apply from "./Pages/Apply/sections/Apply.jsx"
 import ComingSoon from "./Pages/ComingSoon/ComingSoon.jsx"
 import Links from "./Pages/Links/Links.jsx"
+import Main from "./Pages/Main/Main.jsx"
+import Primer from "./Pages/Primer/Primer.jsx"
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import { Analytics } from '@vercel/analytics/react';
 import '@/components/SparkleCursor.css';
@@ -16,6 +18,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<ComingSoon/>}/>
+          <Route path="/primer" element={<Primer/>}/>
           <Route path="/apply" element={<Apply/>}/>
           <Route path="/links" element={<Links/>}/>
         </Routes>
