15337e5515c396e1ab5fa849b13f585d6dad492c
Johan Marlo Cabili <jmtcabili@gmail.com>
2026-03-03 23:05:06 +0800

    fix: removed batch2 banner

--- src/Pages/Primer/Primer.jsx
+++ src/Pages/Primer/Primer.jsx
@@ -3,7 +3,6 @@ import PackagesSection from "./components/PackagesSection";
 import TimelineSection from "./components/TimelineSection";
 import PaymentDeadlinesSection from "./components/PaymentDeadlinesSection";
 import RegistrationStepsSection from "./components/RegistrationStepsSection";
-import StickerBanner from "./components/StickerBanner";
 import Navbar from '@/components/Navbar';
 
 const primerNavLinks = [
@@ -17,7 +16,6 @@ export default function Primer() {
   return (
     <main>
       <Navbar links={primerNavLinks} />
-      <StickerBanner/>
       <HeroSection />
       <PackagesSection />
       <TimelineSection />
