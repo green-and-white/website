45dc4bf5d9e4726aeae2b580bba3f0565248162e
Alvinator24 <alvinalvin3065@gmail.com>
2026-05-11 11:06:41 +0800

    fix: change font

--- src/Pages/T3Apply/Sections/About.tsx
+++ src/Pages/T3Apply/Sections/About.tsx
@@ -9,13 +9,13 @@ function About() {
           <div className="mt-10 h-2 w-15 bg-red-600"></div>
         </div>
         <div className="flex items-start gap-4 max-w-105">
-        <div className="w-2 h-35 bg-green-700 ml-5"></div>
-          <p className="text-xl mt-10 text-green-700">
-            Green & White is the official yearbook publication of De La Salle University.
-          </p>
+        <div className="w-3 h-35 bg-green-700 ml-5"></div>
+        <p className="text-xl mt-10 text-green-700 font-libre-caslon-condensed font-bold">
+          Green & White is the official yearbook publication of De La Salle University.
+        </p>
         </div>
       </div>
-      <div className='max-w-120 ml-186 mt-10'>
+      <div className='max-w-140 ml-186 mt-10 font-dm-mono'>
         Celebrating its 101st year, the publication lives as the record-keeper of Lasallian memories. From portraits to pages, Green & White brings the Lasallian community together and etches their place in time through the yearbook.
       </div>
     </section>
