db9a615a4f8c7ad83a9ceb7b364429a97c0c2449
Alvinator24 <alvinalvin3065@gmail.com>
2026-05-11 10:56:06 +0800

    feat: create about page

--- src/Pages/T3Apply/Sections/About.tsx
+++ src/Pages/T3Apply/Sections/About.tsx
@@ -2,8 +2,22 @@ import React from 'react'
 
 function About() {
   return (
-    <section className="min-h-screen">
-      <h1>About</h1>
+    <section className="min-h-screen bg-[#F5F0E8] flex flex-col">
+      <div className='flex justify-center mt-50 items-start gap-10'>
+        <div className="flex flex-col">
+          <h1 className="font-micu text-7xl">ABOUT US</h1>
+          <div className="mt-10 h-2 w-15 bg-red-600"></div>
+        </div>
+        <div className="flex items-start gap-4 max-w-105">
+        <div className="w-2 h-35 bg-green-700 ml-5"></div>
+          <p className="text-xl mt-10 text-green-700">
+            Green & White is the official yearbook publication of De La Salle University.
+          </p>
+        </div>
+      </div>
+      <div className='max-w-120 ml-186 mt-10'>
+        Celebrating its 101st year, the publication lives as the record-keeper of Lasallian memories. From portraits to pages, Green & White brings the Lasallian community together and etches their place in time through the yearbook.
+      </div>
     </section>
   )
 }
