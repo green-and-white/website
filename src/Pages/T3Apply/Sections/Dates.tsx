add26156294f16c729ba00014c944079d98190f3
Blu-Git <62788833+Blu-Git@users.noreply.github.com>
2026-05-12 17:41:46 +0800

    feat(important-date): add initial styles

--- src/Pages/T3Apply/Sections/Dates.tsx
+++ src/Pages/T3Apply/Sections/Dates.tsx
@@ -2,24 +2,30 @@ import DateCard from "../../../components/t3-recweek/DateCard"
 
 function Dates() {
   return (
-    <section className="min-h-screen">
-      <h1>important dates</h1>
-      <DateCard
-        title="COMMITTEE EXAMS" 
-        dateRange="May 25 to 27"
-      />
-      <DateCard 
-        title="PRACTICAL EXAMS"
-        dateRange="May 25 to 29"
-      />
-      <DateCard
-        title="COMMITTEE HEAD INTERVIEW"
-        dateRange="May 28 to June 2"
-      />
-      <DateCard
-        title="EXECTUIVE EDITOR INTERVIEW"
-        dateRange="June 1 to June 5"
-      /> 
+    <section className="min-h-screen flex flex-col justify-center px-6 bg-[#E8E4DD]">
+      <h1 className="font-micu text-[#B34865] text-5xl text-center mb-24">
+        important dates
+      </h1>
+
+      <div className="flex flex-col gap-3">
+        <DateCard
+          title="COMMITTEE EXAMS" 
+          dateRange="May 25 to 27"
+        />
+        <DateCard 
+          title="PRACTICAL EXAMS"
+          dateRange="May 25 to 29"
+        />
+        <DateCard
+          title="COMMITTEE HEAD INTERVIEW"
+          dateRange="May 28 to June 2"
+        />
+        <DateCard
+          title="EXECTUIVE EDITOR INTERVIEW"
+          dateRange="June 1 to June 5"
+        /> 
+      </div> 
+       
     </section>
   )
 }
--- src/components/t3-recweek/DateCard.tsx
+++ src/components/t3-recweek/DateCard.tsx
@@ -3,9 +3,13 @@ export default function DateCard(
   { title, dateRange }:{ title: string; dateRange: string}
 ) {
   return (
-    <div>
-      <h3>{ title }</h3>
-      <p>{dateRange}</p> 
+    <div className="flex flex-col border-b-2 border-b-[#B34865] pb-3 px-2">
+      <h3 className="text-[#B34865] font-bold text-xl">
+        { title }
+      </h3>
+      <p className="text-[#48B39A] text-lg">
+        { dateRange }
+      </p> 
     </div> 
   )
 }
