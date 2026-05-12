e4f22aecf8b51f41002ff12b951513bad6d7d56a
Blu-Git <62788833+Blu-Git@users.noreply.github.com>
2026-05-12 23:03:06 +0800

    feat(date): add hover animations

--- src/components/t3-recweek/DateCard.tsx
+++ src/components/t3-recweek/DateCard.tsx
@@ -3,11 +3,11 @@ export default function DateCard(
   { title, dateRange }: { title: string; dateRange: string }
 ) {
   return (
-    <div className="flex flex-col md:flex-row md:items-center md:justify-between border-b-2 border-b-[#B34865] px-2 py-4">
-      <h3 className="font-futura-bold text-[#B34865] font-bold text-xl md:text-lg lg:text-xl">
+    <div className="group flex flex-col md:flex-row md:items-center md:justify-between border-b-2 border-b-[#B34865] px-2 py-4 cursor-pointer">
+      <h3 className="font-futura-bold text-[#B34865] font-bold text-xl md:text-lg lg:text-xl transition-all duration-300 group-hover:text-2xl md:group-hover:text-xl lg:group-hover:text-2xl">
         {title}
       </h3>
-      <p className="font-futura-bold text-[#48B39A] text-lg md:text-base lg:text-lg">
+      <p className="font-futura-bold text-[#48B39A] text-lg md:text-base lg:text-lg transition-all duration-300 group-hover:text-xl md:group-hover:text-lg lg:group-hover:text-xl">
         {dateRange}
       </p>
     </div>
