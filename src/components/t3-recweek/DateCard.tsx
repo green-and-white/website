3f42e07ddd40130e41ff0971b786256a661fdfe4
Blu-Git <62788833+Blu-Git@users.noreply.github.com>
2026-05-12 17:55:56 +0800

    feat(importand-dates): add responsive styles

--- src/components/t3-recweek/DateCard.tsx
+++ src/components/t3-recweek/DateCard.tsx
@@ -1,15 +1,15 @@
 
 export default function DateCard(
-  { title, dateRange }:{ title: string; dateRange: string}
+  { title, dateRange }: { title: string; dateRange: string }
 ) {
   return (
-    <div className="flex flex-col border-b-2 border-b-[#B34865] pb-3 px-2">
-      <h3 className="text-[#B34865] font-bold text-xl">
-        { title }
+    <div className="flex flex-col md:flex-row md:items-center md:justify-between border-b-2 border-b-[#B34865] px-2 py-4">
+      <h3 className="text-[#B34865] font-bold text-xl md:text-lg lg:text-xl">
+        {title}
       </h3>
-      <p className="text-[#48B39A] text-lg">
-        { dateRange }
-      </p> 
-    </div> 
+      <p className="text-[#48B39A] text-lg md:text-base lg:text-lg">
+        {dateRange}
+      </p>
+    </div>
   )
 }
