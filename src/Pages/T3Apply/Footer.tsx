c91f6edf0adef4efed3cb088d0fe52cfb4dbcca5
Johan Marlo Cabili <jmtcabili@gmail.com>
2026-05-16 10:48:49 +0800

    refactor: footer - links on buttons - bg from cloudinary

--- src/Pages/T3Apply/Footer.tsx
+++ src/Pages/T3Apply/Footer.tsx
@@ -36,13 +36,16 @@ function Footer() {
   const whiteLogo = T3_ASSETS.logos.find(
     (logo) => logo.public_id === "Vertical_-_White_elepk2"
   ) || { public_id: "Vertical_-_White_elepk2", display_name: "Vertical - White" };
-
+ 
+  const heroImage = T3_ASSETS.images.find(
+    (img) => img.public_id ==="Hero_tcg8tg"
+  ) || { public_id: "Hero_tcg8tg", display_name: "Hero" };
 
   return (
-    <section className="py-8 flex-col flex font-futura-bold justify-center items-center gap-[30px] bg-black">
+    <section className="relative px-6 md:px-12 py-16 md:py-24 flex-col flex font-futura-bold justify-center items-center gap-[30px]">
       <CloudinaryImage
-        publicId="Hero_tcg8tg"
-        alt="Hero Background"
+        publicId={heroImage.public_id}
+        alt={heroImage.display_name}
         className="absolute inset-0 w-full h-full object-cover z-0 filter brightness-[0.9]"
         width={1000}
       />
@@ -61,17 +64,21 @@ function Footer() {
         <Links />
       </div>
       {/* Buttons */}
-      <div className="flex flex-row gap-4">
+      <div className="flex flex-row gap-4 z-2">
         <div className="flex flex-col items-center p-4 bg-white hover:bg-app-green hover:text-app-white transition-colors ease-in-out text-app-green font-futura-extrabold text-xl">
-          main website
+          {/* main website */}
+          {/* redirect to base route / */}
+          <RouterLink to="/">main website</RouterLink>
         </div>
         <div className="flex flex-col items-center p-4 bg-white hover:bg-app-green hover:text-app-white transition-colors ease-in-out text-app-green font-futura-extrabold text-xl">
-          back to top
+          {/* back to top */}
+          {/* scroll to top */}
+          <a href = "#hero">back to top</a>
         </div>
       </div>
 
       {/* credits */}
-      <div className="text-center text-white">
+      <div className="text-center text-white z-2">
         <div className='font-futura-bold'>
           Website by: &nbsp;
           <span className='font-futura-extrabold'>
